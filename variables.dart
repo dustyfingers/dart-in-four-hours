4ae64547ad29ef6e1b08af1a49c8629ed4e88188
lou <dustyfingers01257@gmail.com>
2021-04-17 12:01:58 -0600

    learned about the basic data types in dart

--- variables.dart
+++ variables.dart
@@ -1,8 +1,15 @@
 void main() {
+  // data types!
+  // Numbers(int, double), String, bool, List, Set, Map, Rune, Symbol and teh value null (has a type of Null)
   String characterName = "Louie";
   int characterAge = 24;
+  double rawMagicValue = 3.14;
+  bool isRegistered = true;
+
   print("There was a man named ${characterName}.");
   print("He was ${characterAge} years old.");
   print("He really liked the name ${characterName}.");
   print("He also mildly liked being ${characterAge}.");
+  print("He has a raw magic value of ${rawMagicValue}");
+  print("Registered? ${isRegistered}");
 }
