f86e1800c9b346fecd4ee199499138bb0707a611
lou <dustyfingers01257@gmail.com>
2021-04-17 11:56:53 -0600

    learned about super basic vars in dart

---
+++ variables.dart
@@ -0,0 +1,8 @@
+void main() {
+  String characterName = "Louie";
+  int characterAge = 24;
+  print("There was a man named ${characterName}.");
+  print("He was ${characterAge} years old.");
+  print("He really liked the name ${characterName}.");
+  print("He also mildly liked being ${characterAge}.");
+}
