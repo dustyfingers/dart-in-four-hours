583f1945198d7781c1a9c0a4081c7cdef1355bfa
lou <dustyfingers01257@gmail.com>
2021-04-17 12:25:21 -0600

    learned about basic path operations in dart

--- variables.dart
+++ variables.dart
@@ -1,6 +1,8 @@
+import "dart:math";
+
 void main() {
   // data types!
-  // Numbers(int, double), String, bool, List, Set, Map, Rune, Symbol and teh value null (has a type of Null)
+  // Numbers(int, double), String, bool (true or false), List, Set, Map, Rune, Symbol and teh value null (has a type of Null)
   String characterName = "Louie";
   int characterAge = 24;
   double rawMagicValue = 3.14;
@@ -37,4 +39,18 @@ void main() {
 
   // check if string contains
   print(exampleStringTwo.contains("xx"));
+
+  // numbers
+  // + - / * % and order of operations work as expected!
+
+  // when dividing two ints, get double back!
+  print(4 / 3);
+
+  // += -= *= /= work as expected (operation => reassignment)
+  print(characterAge *= 3);
+  print(rawMagicValue += 4);
+
+  // math library for more advanced stuff!
+  print(min(characterAge, rawMagicValue));
+  print(sqrt(144));
 }
